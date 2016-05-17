f22eddf330f818bacd435997df637a81ad41d1c3
Alberto Pellizzon <alberto_pellizzon@gruppopam.it>
2016-05-17 13:08:20 +0200

    Use Map with Roman Numerals

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -1,27 +1,27 @@
 module Roman where
 
-  import Data.List
   import Data.Monoid(mappend)
   import Data.Maybe(fromJust)
+  import Data.Map.Lazy
 
   numerals :: Int -> String
-  signs :: [(String, Int)]
+  signs :: Map Int String
 
-  signs = [("M", 1000),
-           ("CM", 900),
-           ("D",  500),
-           ("CD", 400),
-           ("C",  100),
-           ("XC",  90),
-           ("L",   50),
-           ("XL",  40),
-           ("X",   10),
-           ("IX",   9),
-           ("V",    5),
-           ("IV",   4),
-           ("I",    1)]
+  signs = fromList [(1000, "M"),
+                    ( 900, "CM"),
+                    ( 500, "D"),
+                    ( 400, "CD"),
+                    ( 100, "C"),
+                    (  90, "XC"),
+                    (  50, "L"),
+                    (  40, "XL"),
+                    (  10, "X"),
+                    (   9, "IX"),
+                    (   5, "V"),
+                    (   4, "IV"),
+                    (   1, "I")]
 
   numerals number
     | number == 0 = ""
-    | otherwise = key `mappend` numerals (number - value)
-    where (key, value) = fromJust $ find (flip (<=) number . snd) signs
+    | otherwise = value `mappend` numerals (number - key)
+    where (key, value) = fromJust $ lookupLE number signs
