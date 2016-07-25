16db10e1d41dd933da8859a48bb5fd3cbe13d3ab
Alberto Pellizzon <alberto_pellizzon@gruppopam.it>
2016-07-25 16:24:05 +0200

    Add Haskell robot-simulator

---
+++ haskell/robot-simulator/Robot.hs
@@ -0,0 +1,60 @@
+module Robot
+    (
+      Robot(..),
+      Bearing(..),
+      mkRobot,
+      turnLeft,
+      turnRight,
+      simulate
+    ) where
+
+  type Path = String
+
+  type Coordinates = (Integer, Integer)
+  data Bearing = South | East | North | West deriving (Show, Eq, Enum)
+  data Robot = Robot {
+    bearing :: Bearing,
+    coordinates :: Coordinates
+  } deriving (Show, Eq)
+
+  mkRobot :: Bearing -> Coordinates -> Robot
+  mkRobot = Robot
+
+  turnLeft :: Bearing -> Bearing
+  turnLeft w = case w of
+    South -> East
+    North -> West
+    West -> South
+    East -> North
+
+  turnRight :: Bearing -> Bearing
+  turnRight w = turnLeft $ toEnum opposite
+    where opposite = (fromEnum w + 2) `mod` 4
+
+  simulate :: Robot -> Path -> Robot
+  simulate r [] = r
+  simulate r (s:ss) = case s of
+    'R' -> simulate' $ withSameCoordinates (turnRight currentBearing)
+    'L' -> simulate' $ withSameCoordinates (turnLeft  currentBearing)
+    'A' -> simulate' $ advance r
+    where
+      simulate' = flip simulate ss
+      withSameCoordinates = flip mkRobot $ coordinates r
+      currentBearing = bearing r
+
+  xFor :: Robot -> Integer
+  xFor r = case bearing r of
+    East -> fst (coordinates r) + 1
+    West -> fst (coordinates r) - 1
+    _ -> fst $ coordinates r
+
+  yFor :: Robot -> Integer
+  yFor r = case bearing r of
+    North -> snd (coordinates r) + 1
+    South -> snd (coordinates r) - 1
+    _ -> snd  (coordinates r)
+
+  advance :: Robot -> Robot
+  advance r = withSameBearing (xFor r, yFor r)
+    where
+      withSameBearing = mkRobot (bearing r)
